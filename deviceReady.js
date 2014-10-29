ab0c5ea05bf0d09eae929c395dcfa36037be124b
Soivi <soivi.lauri@gmail.com>
2014-10-29 19:36:13 +0200

    refactored and moved variables all levels

--- deviceReady.js
+++ deviceReady.js
@@ -1,3 +1,10 @@
+	var wantedNumber;
+	var db = window.openDatabase("Database", "1.0", "Cordova", 200000);
+	var randomArrayNumbers = new Array();
+	var yourBestTime = 0;
+	var totalTime = 0;
+	var correctAnswers = -1;
+
 db.transaction(queryDBVibrationOff, errorCBVibrationOff);
 makeTable();
 $('.guessbutton').mousedown(function(e) {
